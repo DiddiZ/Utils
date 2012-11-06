bc5b8fc129d2ce3c86c9d011036f8b4d50afba7e
DiddiZ <robbzkupper@web.de>
2012-11-06 17:11:56 +0100

    Added some javadoc annotations

--- src/de/diddiz/utils/ImageUtils.java
+++ src/de/diddiz/utils/ImageUtils.java
@@ -7,7 +7,15 @@ import java.nio.ByteBuffer;
 
 public class ImageUtils
 {
-
+	/**
+	 * Creates a new resized BufferedImage with the supplied size for the longer edge
+	 * 
+	 * @param img
+	 * BufferedImage to resize
+	 * @param size
+	 * Length of the longer edge
+	 * @return A new resized image
+	 */
 	public static BufferedImage resize(BufferedImage img, int size) {
 		final int width = img.getWidth(), height = img.getHeight();
 		final int nWidth = width > height ? size : (int)((float)width / height * size);
@@ -15,20 +23,28 @@ public class ImageUtils
 		return resize(img, nWidth, nHeight);
 	}
 
-	public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+	/**
+	 * Creates a new resized BufferedImage with the supplied dimensions
+	 * 
+	 * @return A new resized image
+	 */
+	public static BufferedImage resize(BufferedImage img, int newWidth, int newHeight) {
 		final int w = img.getWidth();
 		final int h = img.getHeight();
-		final BufferedImage dimg = new BufferedImage(newW, newH, img.getType());
+		final BufferedImage dimg = new BufferedImage(newWidth, newHeight, img.getType());
 		final Graphics2D g = dimg.createGraphics();
 		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 		g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		g.drawImage(img, 0, 0, newW, newH, 0, 0, w, h, null);
+		g.drawImage(img, 0, 0, newWidth, newHeight, 0, 0, w, h, null);
 		g.dispose();
 		return dimg;
 	}
 
+	/**
+	 * Generates a BufferedImage from a ByteByffer. The ByteBuffer is assumed to be RGB formatted.
+	 */
 	public static BufferedImage toImage(ByteBuffer bb, int width, int height) {
 		final BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		for (int x = 0; x < width; x++)

1e6ebdc1c6bc8f22bbab1d06ec98b496f00db1e6
DiddiZ <robbzkupper@web.de>
2013-02-13 23:46:22 +0100

    Added ReversedListIterable

---
+++ src/de/diddiz/utils/iter/ReversedListIterable.java
@@ -0,0 +1,38 @@
+package de.diddiz.utils.iter;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class ReversedListIterable<T> implements Iterable<T>
+{
+	private final List<T> list;
+
+	public ReversedListIterable(List<T> list) {
+		this.list = list;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+			private int pos = list.size() - 1;
+
+			@Override
+			public boolean hasNext() {
+				return pos >= 0;
+			}
+
+			@Override
+			public T next() throws NoSuchElementException {
+				if (pos >= 0)
+					return list.get(pos--);
+				throw new NoSuchElementException();
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+}

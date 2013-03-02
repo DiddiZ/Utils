a5647c824e856535c87eac18c31b693a9b29ee78
DiddiZ <robbzkupper@web.de>
2013-03-02 11:29:54 +0100

    Added CharPredicates.decimals()

--- src/de/diddiz/utils/predicates/CharPredicates.java
+++ src/de/diddiz/utils/predicates/CharPredicates.java
@@ -2,25 +2,66 @@ package de.diddiz.utils.predicates;
 
 public class CharPredicates
 {
+	/**
+	 * Allows the numbers from {@code 0} to {@code 9} as well as dots ({@code .}) and signs ({@code +-}).
+	 */
+	public static CharPredicate decimals() {
+		return DecimalCharPredicate.INSTANCE;
+	}
+
+	/**
+	 * Allows the numbers from {@code 0} to {@code 9}.
+	 */
 	public static CharPredicate digitsOnly() {
-		return new CharPredicate() {
-			@Override
-			public boolean apply(char input) {
-				switch (input) {
-					case '0':
-					case '1':
-					case '2':
-					case '3':
-					case '4':
-					case '5':
-					case '6':
-					case '7':
-					case '8':
-					case '9':
-						return true;
-				}
-				return false;
+		return DigitCharPredicate.INSTANCE;
+	}
+
+	private static class DecimalCharPredicate implements CharPredicate
+	{
+		public static final CharPredicate INSTANCE = new DecimalCharPredicate();
+
+		@Override
+		public boolean apply(char input) {
+			switch (input) {
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+				case '.':
+				case '+':
+				case '-':
+					return true;
 			}
-		};
+			return false;
+		}
+	}
+
+	private static class DigitCharPredicate implements CharPredicate
+	{
+		public static final CharPredicate INSTANCE = new DigitCharPredicate();
+
+		@Override
+		public boolean apply(char input) {
+			switch (input) {
+				case '0':
+				case '1':
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+				case '8':
+				case '9':
+					return true;
+			}
+			return false;
+		}
 	}
 }

a28bb00c4c1c3d118ef029c2bb99ae34f744e81f
DiddiZ <robbzkupper@web.de>
2013-03-05 18:25:13 +0100

    Improved performance of FloatMath.min and max.

--- src/de/diddiz/utils/FloatMath.java
+++ src/de/diddiz/utils/FloatMath.java
@@ -3,10 +3,9 @@ package de.diddiz.utils;
 public class FloatMath
 {
 	/**
-	 * @param val
-	 * @param min
-	 * @param max
-	 * @return min if value is smaller than min, max if value is higher than max, otherwise the value itself. This is equavalien to {@code Math.max(Math.min(value, max), min)} but faster and doesn't require a local variable for value.
+	 * This is equivalent to {@code Math.max(Math.min(value, max), min)} but it's faster and doesn't require a local variable for value.
+	 * 
+	 * @return min if value is smaller than min, max if value is higher than max, otherwise the value itself.
 	 */
 	public static float clamp(float val, float min, float max) {
 		if (val < min)
@@ -16,11 +15,16 @@ public class FloatMath
 		return val;
 	}
 
+	/**
+	 * Computes and returns the largest (nearest to positive infinity) value.
+	 * 
+	 * @throws ArrayIndexOutOfBoundsException When array is empty.
+	 */
 	public static float max(float... arr) {
-		float max = Float.MIN_VALUE;
-		for (final float f : arr)
-			if (f > max)
-				max = f;
+		float max = arr[0];
+		for (int i = 1; i < arr.length; i++)
+			if (arr[i] > max)
+				max = arr[i];
 		return max;
 	}
 
@@ -35,11 +39,16 @@ public class FloatMath
 		return a > c ? a : c;
 	}
 
+	/**
+	 * Computes and returns the smallest (nearest to negative infinity) value.
+	 * 
+	 * @throws ArrayIndexOutOfBoundsException When array is empty.
+	 */
 	public static float min(float... arr) {
-		float min = Float.MAX_VALUE;
-		for (final float f : arr)
-			if (f < min)
-				min = f;
+		float min = arr[0];
+		for (int i = 1; i < arr.length; i++)
+			if (arr[i] < min)
+				min = arr[i];
 		return min;
 	}
 

655ed97152d5662d789af2728ebadaf76ee623a9
DiddiZ <robbzkupper@web.de>
2013-06-11 21:32:27 +0200

    Added ByteArrayPool.

---
+++ src/de/diddiz/utils/arrays/ByteArrayPool.java
@@ -0,0 +1,46 @@
+package de.diddiz.utils.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Pool of byte arrays with a fixed length. To prevent pollution this pool has a maximum number of kept arrays.
+ */
+public class ByteArrayPool
+{
+	private final int arrayLength;
+	private final int maxPoolSize;
+	private final List<byte[]> pool;
+
+	/**
+	 * @param arrayLength All arrays are of this length.
+	 * @param maxPoolSize Number of kept arrays.
+	 */
+	public ByteArrayPool(int arrayLength, int maxPoolSize) {
+		this.arrayLength = arrayLength;
+		this.maxPoolSize = maxPoolSize;
+
+		pool = new ArrayList<>(maxPoolSize);
+	}
+
+	/**
+	 * Returns an array to the pool. If the pool is full the array is forsaken.
+	 * 
+	 * @throws IllegalArgumentException If the supplied array has a different length than this pool requires
+	 */
+	public void add(byte[] array) throws IllegalArgumentException {
+		if (array.length != arrayLength)
+			throw new IllegalArgumentException("Supplied array has a length of " + array.length + " while this pool enforces a length of " + arrayLength);
+		if (pool.size() < maxPoolSize)
+			pool.add(array);
+	}
+
+	/**
+	 * Either retrieves an array from the pool or creates a new one if the pool is empty.
+	 */
+	public byte[] get() {
+		if (pool.size() > 0)
+			return pool.remove(pool.size() - 1);
+		return new byte[arrayLength];
+	}
+}

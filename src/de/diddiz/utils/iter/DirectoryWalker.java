650446ec0723c55add09f9708e74e70e663e8a85
DiddiZ <robbzkupper@web.de>
2013-08-31 21:22:49 +0200

    Added DirectoryWalker.

---
+++ src/de/diddiz/utils/iter/DirectoryWalker.java
@@ -0,0 +1,59 @@
+package de.diddiz.utils.iter;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * Iterates over all sub-directories of a folder.
+ */
+public class DirectoryWalker implements Iterable<File>
+{
+	private final File[] roots;
+
+	/**
+	 * The {@code Iterator} will iterate over the directory itself and all sub-folders in all sub-directories.
+	 * 
+	 * The {@code Iterator} only returns existing directories.
+	 * 
+	 * @param roots May be a directory or point to nothing.
+	 */
+	public DirectoryWalker(File... roots) {
+		this.roots = roots;
+	}
+
+	@Override
+	public Iterator<File> iterator() {
+		return new DirectoryWalkerIterator(roots);
+	}
+
+	private static class DirectoryWalkerIterator extends ComputeNextIterator<File>
+	{
+		private final LinkedList<File> folders = new LinkedList<>();
+
+		public DirectoryWalkerIterator(File... roots) {
+			for (final File root : roots)
+				if (root.isDirectory())
+					folders.add(root);
+			computeFirst();
+		}
+
+		@Override
+		protected File computeNext() {
+			if (folders.size() > 0) {
+				final File folder = folders.poll();
+
+				// Add all sub dirs
+				final File[] subFiles = folder.listFiles();
+				if (subFiles != null && subFiles.length > 0)
+					for (final File file : subFiles)
+						if (file.isDirectory())
+							folders.add(file);
+
+				return folder;
+			}
+			// We have no more folders
+			return null;
+		}
+	}
+}

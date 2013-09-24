26177051881d737d57256f289661737b1e62ffcb
DiddiZ <robbzkupper@web.de>
2013-09-24 21:47:10 +0200

    Documented FileFilters.endsWith().

--- src/de/diddiz/utils/FileFilters.java
+++ src/de/diddiz/utils/FileFilters.java
@@ -5,6 +5,11 @@ import java.io.FilenameFilter;
 
 public final class FileFilters
 {
+	/**
+	 * Simple {@link FilenameFilter} that matches {@code Files} agains an extension
+	 * 
+	 * @param extension Case insensitive.
+	 */
 	public static FilenameFilter endsWith(final String extension) {
 		return new FilenameFilter() {
 			String ext = extension.toLowerCase();

68fd2f3c27714d1821ee3e30334be91d9c12fbd9
DiddiZ <robbzkupper@web.de>
2013-09-30 01:31:43 +0200

    Added Factories.create(V, ParametrizedFactory<T, V>...).

--- src/de/diddiz/utils/factories/Factories.java
+++ src/de/diddiz/utils/factories/Factories.java
@@ -1,9 +1,22 @@
 package de.diddiz.utils.factories;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 public final class Factories
 {
+	/**
+	 * Creates a {@code T} from an arbitrary number of factories using the same {@code V}
+	 */
+	@SafeVarargs
+	public static <T, V> List<T> create(V v, ParametrizedFactory<T, V>... factories) {
+		final List<T> list = new ArrayList<>(factories.length);
+		for (int i = 0; i < factories.length; i++)
+			list.add(factories[i].create(v));
+		return list;
+	}
+
 	public static <T> Factory<T> fixedFactory(final T value) {
 		return new Factory<T>() {
 			@Override

492ce035be6f3c54c8135d6966eef1f8c5555276
DiddiZ <robbzkupper@web.de>
2013-11-22 18:57:56 +0100

    Added SimpleDatabases;

---
+++ src/de/diddiz/utils/sql/databases/SimpleDatabase.java
@@ -0,0 +1,51 @@
+package de.diddiz.utils.sql.databases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * A simple class containing a {@link Connection}. {@code SimpleDatabase} is meant to be used for local databases like sqlite or H2.
+ * 
+ * Sub-classes should add methods to handle common queries, so the actual code doesn't have to handle SQL stuff.
+ */
+public abstract class SimpleDatabase implements AutoCloseable
+{
+	/**
+	 * Do never close this manually. When the database is no longer needed use {@link #close()} instead.
+	 */
+	protected final Connection conn;
+
+	public SimpleDatabase(String driver, String url, String user, String pw) throws SQLException {
+		try {
+			Class.forName(driver);
+		} catch (final ClassNotFoundException ex) {
+			throw new AssertionError(ex);
+		}
+		conn = DriverManager.getConnection(url, user, pw);
+
+		// Initialize database
+		try (Statement st = conn.createStatement()) {
+			initializeDatabase(st);
+		}
+	}
+
+	@Override
+	public void close() throws SQLException {
+		conn.close();
+	}
+
+	/**
+	 * This method is called every time a connection is created.
+	 * 
+	 * Please use <code>CREATE TABLE IF NOT EXISTS [...]</code>.
+	 */
+	protected abstract void initializeDatabase(Statement st);
+
+	protected static String escape(String str) {
+		if (str.indexOf('\'') > 0)
+			str = str.replace("'", "''");
+		return str;
+	}
+}
---
+++ src/de/diddiz/utils/sql/databases/SimpleH2Database.java
@@ -0,0 +1,15 @@
+package de.diddiz.utils.sql.databases;
+
+import java.io.File;
+import java.sql.SQLException;
+
+public abstract class SimpleH2Database extends SimpleDatabase
+{
+	public SimpleH2Database(File databaseFile) throws SQLException {
+		this(databaseFile, "", "");
+	}
+
+	public SimpleH2Database(File databaseFile, String user, String pw) throws SQLException {
+		super("org.h2.Driver", "jdbc:h2:" + databaseFile.getPath(), user, pw);
+	}
+}

c27521e059d89cbab75485c34f75647b479c03ea
DiddiZ <robbzkupper@web.de>
2013-11-27 23:26:37 +0100

    Replaced CharPredicate with Predicate<Character>;

--- src/de/diddiz/utils/predicates/CharPredicate.java
+++
@@ -1,6 +0,0 @@
-package de.diddiz.utils.predicates;
-
-public interface CharPredicate
-{
-	public boolean apply(char input);
-}
--- src/de/diddiz/utils/predicates/CharPredicates.java
+++ src/de/diddiz/utils/predicates/CharPredicates.java
@@ -1,69 +1,71 @@
 package de.diddiz.utils.predicates;
 
+import com.google.common.base.Predicate;
+
 public final class CharPredicates
 {
 	/**
 	 * Allows the numbers from {@code 0} to {@code 9} as well as dots ({@code .}) and signs ({@code +-}).
 	 */
-	public static CharPredicate decimals() {
+	public static Predicate<Character> decimals() {
 		return DecimalCharPredicate.INSTANCE;
 	}
 
 	/**
 	 * Allows the numbers from {@code 0} to {@code 9}.
 	 */
-	public static CharPredicate digitsOnly() {
+	public static Predicate<Character> digitsOnly() {
 		return DigitCharPredicate.INSTANCE;
 	}
 
-	private static class DecimalCharPredicate implements CharPredicate
+	private static class DecimalCharPredicate implements Predicate<Character>
 	{
-		public static final CharPredicate INSTANCE = new DecimalCharPredicate();
+		private static final Predicate<Character> INSTANCE = new DecimalCharPredicate();
 
 		@Override
-		public boolean apply(char input) {
-			switch (input) {
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-				case '.':
-				case '+':
-				case '-':
-					return true;
-				default:
-					return false;
-			}
+		public boolean apply(Character input) {
+			if (input != null)
+				switch (input) {
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+					case '.':
+					case '+':
+					case '-':
+						return true;
+				}
+			return false;
 		}
 	}
 
-	private static class DigitCharPredicate implements CharPredicate
+	private static class DigitCharPredicate implements Predicate<Character>
 	{
-		public static final CharPredicate INSTANCE = new DigitCharPredicate();
+		private static final Predicate<Character> INSTANCE = new DigitCharPredicate();
 
 		@Override
-		public boolean apply(char input) {
-			switch (input) {
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-					return true;
-				default:
-					return false;
-			}
+		public boolean apply(Character input) {
+			if (input != null)
+				switch (input) {
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+						return true;
+				}
+			return false;
 		}
 	}
 }

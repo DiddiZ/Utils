9e5b143a4d11970019ce8c5e7abe2aa6a389ad08
DiddiZ <robbzkupper@web.de>
2013-11-27 22:53:46 +0100

    Replaced IntFunction<T> with Fuction<T, integer>;

--- src/de/diddiz/utils/functions/IntFunction.java
+++
@@ -1,9 +0,0 @@
-package de.diddiz.utils.functions;
-
-public interface IntFunction<T>
-{
-	/**
-	 * Equivalent to {@link com.google.common.base.Function Function} for {@code int}.
-	 */
-	public int apply(T input);
-}

3b3361e899ebcd8fbfb73a30a2d416e856b9ec5b
DiddiZ <robbzkupper@web.de>
2013-12-11 16:48:10 +0100

    Added SwingUtils.setEnabledRecursively;

---
+++ src/de/diddiz/utils/SwingUtils.java
@@ -0,0 +1,20 @@
+package de.diddiz.utils;
+
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.RootPaneContainer;
+
+public class SwingUtils
+{
+	/**
+	 * Disables/enables the components and all of its children.
+	 */
+	public static void setEnabledRecursively(Component component, boolean enabled) {
+		component.setEnabled(enabled);
+		if (component instanceof RootPaneContainer)
+			setEnabledRecursively(((RootPaneContainer)component).getContentPane(), enabled);
+		if (component instanceof Container)
+			for (final Component c : ((Container)component).getComponents())
+				setEnabledRecursively(c, enabled);
+	}
+}

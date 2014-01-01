5a3ffbe8655d1795d90805c8e59ea44ecc6f7adb
DiddiZ <robbzkupper@web.de>
2014-01-01 16:31:48 +0100

    Added Counter and CountMap;

---
+++ src/de/diddiz/utils/math/CountMap.java
@@ -0,0 +1,24 @@
+package de.diddiz.utils.math;
+
+import java.util.HashMap;
+
+public class CountMap<K> extends HashMap<K, Counter>
+{
+	public void increment(K key) {
+		Counter counter = get(key);
+		if (counter == null) {
+			counter = new Counter(0);
+			put(key, counter);
+		}
+		counter.increment();
+	}
+	
+	public void increment(K key, int increment) {
+		Counter counter = get(key);
+		if (counter == null) {
+			counter = new Counter(0);
+			put(key, counter);
+		}
+		counter.increment(increment);
+	}
+}
---
+++ src/de/diddiz/utils/math/Counter.java
@@ -0,0 +1,47 @@
+package de.diddiz.utils.math;
+
+import java.util.Map;
+
+/**
+ * Simple class representing an integer counter.
+ * <p>
+ * This class is intended to be used in {@link Map Maps} as value in place of an Integer to avoid using {@link Map#put(Object, Object) put} for every value update.
+ * 
+ * <pre>
+ * Map&lt;String, Integer&gt; map = new HashMap&lt;&gt;();
+ * Integer value = map.get(key);
+ * map.put(key, value != null ? value + 1 : 1);
+ * </pre>
+ * 
+ * <pre>
+ * final Map&lt;String, Counter&gt; map = new HashMap&lt;&gt;();
+ * final Counter counter = map.get(key);
+ * if (counter == null) {
+ * 	counter = new Counter(0);
+ * 	map.put(key, counter);
+ * }
+ * counter.increment();
+ * </pre>
+ */
+public class Counter
+{
+	private int value;
+
+	public Counter() {}
+
+	public Counter(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void increment() {
+		value++;
+	}
+
+	public void increment(int increment) {
+		value += increment;
+	}
+}

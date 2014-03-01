f1df58ae4f370db1878b17512a66ac169d62dd5c
DiddiZ <robbzkupper@web.de>
2014-03-01 14:54:20 +0100

    Minor comment cleanup;

--- src/de/diddiz/utils/IntMath.java
+++ src/de/diddiz/utils/IntMath.java
@@ -17,9 +17,6 @@ public final class IntMath
 	}
 
 	/**
-	 * @param val
-	 * @param min
-	 * @param max
 	 * @return min if value is smaller than min, max if value is higher than max, otherwise the value itself. This is equavalien to {@code Math.max(Math.min(value, max), min)} but faster and doesn't require a local variable for value.
 	 */
 	public static int clamp(int val, int min, int max) {
